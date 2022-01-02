6119144b0ff7d365b23b4598f4fc5880e8a99639
txoksue <txoksue80@gmail.com>
2022-01-02 18:43:12 +0100

    Added DTOs

---
+++ app/src/main/java/com/paradigma/rickyandmorty/data/repository/remote/api/model/CharacterResponse.kt
@@ -0,0 +1,12 @@
+package com.paradigma.rickyandmorty.data.repository.remote.api.model
+
+
+import com.google.gson.annotations.SerializedName
+
+
+data class CharacterResponse(
+    @SerializedName("info")
+    val info: InfoDTO?,
+    @SerializedName("results")
+    val results: List<CharacterDTO>?
+)
