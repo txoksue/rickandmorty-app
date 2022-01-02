b478d16c1bff92460a3308aef87c8b9d2cf63510
txoksue <txoksue80@gmail.com>
2022-01-02 22:19:39 +0100

    Updated repository dependency injection

--- app/src/main/java/com/paradigma/rickyandmorty/di/RespositoryModule.kt
+++ app/src/main/java/com/paradigma/rickyandmorty/di/RespositoryModule.kt
@@ -3,6 +3,8 @@ package com.paradigma.rickyandmorty.di
 
 import com.paradigma.rickyandmorty.data.repository.remote.characters.CharacterRepositoryImpl
 import com.paradigma.rickyandmorty.data.repository.remote.characters.CharacterRepository
+import com.paradigma.rickyandmorty.data.repository.remote.location.LocationRepository
+import com.paradigma.rickyandmorty.data.repository.remote.location.LocationRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,9 +13,13 @@ import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-abstract class CharacterModule {
+abstract class RespositoryModule {
 
     @Singleton
     @Binds
     abstract fun bindCharacterRepository(impl: CharacterRepositoryImpl): CharacterRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindLocationRepository(impl: LocationRepositoryImpl): LocationRepository
 }
