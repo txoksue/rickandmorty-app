c56a86a7a09f09103cbea09379a9fbbd698d994a
txoksue <txoksue80@gmail.com>
2022-01-02 22:18:02 +0100

    Added locationDTO object

--- app/src/main/java/com/paradigma/rickyandmorty/data/repository/remote/api/RickyAndMortyApi.kt
+++ app/src/main/java/com/paradigma/rickyandmorty/data/repository/remote/api/RickyAndMortyApi.kt
@@ -1,7 +1,8 @@
 package com.paradigma.rickyandmorty.data.repository.remote.api
 
-import android.location.Location
 import com.paradigma.rickyandmorty.data.repository.remote.api.model.CharacterResponse
+import com.paradigma.rickyandmorty.data.repository.remote.api.model.CharacterLocationDTO
+import com.paradigma.rickyandmorty.data.repository.remote.api.model.LocationDTO
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -16,5 +17,5 @@ interface RickyAndMortyApi {
     suspend fun charactersForPage(@Query("page") page: Int): Response<CharacterResponse>
 
     @GET("location/{location}")
-    suspend fun getLocationById(@Path("location") location: String): Response<Location>
+    suspend fun getLocationById(@Path("location") location: String): Response<LocationDTO>
 }
--- app/src/main/java/com/paradigma/rickyandmorty/data/repository/remote/api/model/CharacterDTO.kt
+++ app/src/main/java/com/paradigma/rickyandmorty/data/repository/remote/api/model/CharacterDTO.kt
@@ -17,7 +17,7 @@ data class CharacterDTO @Inject constructor (
     @SerializedName("image")
     val image: String?,
     @SerializedName("location")
-    val location: LocationDTO?,
+    val characterLocation: CharacterLocationDTO?,
     @SerializedName("name")
     val name: String?,
     @SerializedName("origin")
